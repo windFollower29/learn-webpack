211a22ee286784a64bf3df6c0c540ac0865be9d3
HelloAndo <331754520@qq.com>
2019-02-25 23:41:00 +0800

    koa-hmr

--- app.js
+++ app.js
@@ -3,19 +3,30 @@ var express = require('express')
 var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 
+var koaDevMiddleware = require('koa-webpack-dev-middleware')
+
 var config = require('./webpack.config.js')
 var compiler = webpack(config)
 
-// var app = new Koa()
-var app = express()
+var app = new Koa()
+// var app = express()
 
-app.use(webpackDevMiddleware(compiler, {
+app.use(koaDevMiddleware(compiler, {
   publicPath: config.output.publicPath
 }))
 
-app.use(require("webpack-hot-middleware")(compiler, {
+app.use(require("koa-webpack-hot-middleware")(compiler, {
   log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
 }));
+
+
+// app.use(webpackDevMiddleware(compiler, {
+//   publicPath: config.output.publicPath
+// }))
+
+// app.use(require("webpack-hot-middleware")(compiler, {
+//   log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+// }));
 
 app.listen(3000, () => {
   console.log('server is listening on port: ', 3000)
